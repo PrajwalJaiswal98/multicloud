2d6ca5980e77b9c210c9737ff1e1abed0bf2ee2c
PrajwalJaiswal98 <60100023+PrajwalJaiswal98@users.noreply.github.com>
2020-06-13 22:32:24 +0530

    commit1

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+<?php
+echo "<h2>PHP is Fun!</h2>";
+echo "Hello world!<br>";
+echo "I'm about to learn Terraform<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+?> 
+
+</body>
+</html>
